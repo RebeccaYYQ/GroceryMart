b0547118e4c4057e891ac5e3598c07787dff9a64
Rebecca Y <micelover12@gmail.com>
2024-04-25 22:47:27 +1000

    Added category search with navigation menu

--- nav.php
+++ nav.php
@@ -1,21 +1,27 @@
 <header>
     <nav>
        
-        <form action="navSearch.php" method="Post">
-            <ul class="flex">
-                <!-- <select name="sDegree">
+        <form action="navSearch.php" method="POST">
+            <button type="submit" name="category" value="Home">Home</button>    
+            <button type="submit" name="category" value="Frozen">Frozen</button>
+            <button type="submit" name="category" value="Fresh">Fresh</button>
+            <button type="submit" name="category" value="Beverages">Beverages</button>
+            <button type="submit" name="category" value="Household">Household</button>
+            <button type="submit" name="category" value="Pet Food">Pet Food</button>
+            <!-- <ul class="flex">
+                <select name="sDegree">
                     <option>Bachelor</option>
                     <option>Masters</option>
                     <option>PhD</option>
-                </select> -->
+                </select>
 
 
                 <li><a href="index.php" class="navLink">Home</a></li>
-                <li><a href="navSearch.php" class="navLink">Frozen</a></li>
-                <li><a href="navSearch.php" class="navLink">Fresh</a></li>
-                <li><a href="navSearch.php" class="navLink">Beverages</a></li>
-                <li><a href="navSearch.php" class="navLink">Pet Food</a></li>
-            </ul>
+                <li><a href="navSearch.php" class="navLink" name="Frozen">Frozen</a></li>
+                <li><a href="navSearch.php" class="navLink" name="Fresh">Fresh</a></li>
+                <li><a href="navSearch.php" class="navLink" name="Beverages">Beverages</a></li>
+                <li><a href="navSearch.php" class="navLink" name="Pet Food">Pet Food</a></li>
+            </ul> -->
         </form>
     </nav>
 </header>
--- navSearch.php
+++ navSearch.php
@@ -26,39 +26,40 @@
 
 
     <main>
-        <h2>Frozen</h2>
+        <?php
+        $conn = mysqli_connect("localhost", "root", "", "progintas1");
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            exit;
+        }
 
-        <section class="itemGrid flex">
-            <?php
-            $conn = mysqli_connect("localhost", "root", "", "progintas1");
-            if (mysqli_connect_errno()) {
-                echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                exit;
+        $category = $_POST['category'];
+        $query_string = "select * from as1db where product_category = '$category'";
+
+        //set the page title
+        echo "<h2>$category</h2>
+                <section class='itemGrid flex'>";
+
+        $result = mysqli_query($conn, $query_string);
+        $num_rows = mysqli_num_rows($result);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<div class='productItem flex'>
+                        <img src='images/{$row['product_id']}.jpg' class='productItemImage'>
+                        <p class='productItemContent'><b>{$row['product_name']}</b><br>
+                        \${$row['unit_price']} per {$row['unit_quantity']} <br>
+                        Stock: {$row['in_stock']}</p>
+                        <div class='flex'>
+                            <button class='quantityBtn' type='button' onClick='itemGridCart(\"minus\", \"{$row['product_id']}\")'>-</button>
+                            <p class='quantityField' id='{$row['product_id']}Quantity'>0</p>
+                            <button class='quantityBtn' type='button' onClick='itemGridCart(\"plus\", \"{$row['product_id']}\")'>+</button>
+                        </div>
+                    </div>";
             }
-
-            $query_string = "select * from as1db";
-
-            $result = mysqli_query($conn, $query_string);
-            $num_rows = mysqli_num_rows($result);
-            if (mysqli_num_rows($result) > 0) {
-                // output data of each row
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<div class='productItem flex'>
-                            <img src='images/{$row['product_id']}.jpg' class='productItemImage'>
-                            <p class='productItemContent'><b>{$row['product_name']}</b><br>
-                            \${$row['unit_price']} per {$row['unit_quantity']} <br>
-                            Stock: {$row['in_stock']}</p>
-                            <div class='flex'>
-                                <button class='quantityBtn' type='button' onClick='itemGridCart(\"minus\", \"{$row['product_id']}\")'>-</button>
-                                <p class='quantityField' id='{$row['product_id']}Quantity'>0</p>
-                                <button class='quantityBtn' type='button' onClick='itemGridCart(\"plus\", \"{$row['product_id']}\")'>+</button>
-                            </div>
-                        </div>";
-                        //echo "<pre>" . print_r($row, true) . "</pre>";
-                }
-            }
-            mysqli_close($conn);
-            ?>
+        }
+        mysqli_close($conn);
+        ?>
         </section>
     </main>
 </body>
