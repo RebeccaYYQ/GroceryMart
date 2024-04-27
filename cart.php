2e14fd3474d86a3d60dfaf0cbe4615b38e28c593
Rebecca Y <micelover12@gmail.com>
2024-04-28 00:07:36 +1000

    Minor style adjust - adding clearer <head> titles

--- cart.php
+++ cart.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="script.js"></script>
-    <title>Grocery Mart</title>
+    <title>Grocery Mart | Shopping Cart</title>
 </head>
 
 <body>
